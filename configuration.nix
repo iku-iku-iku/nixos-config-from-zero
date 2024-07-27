c588ca3e44634bf4c0511abc6ef5ea22ce2d8b66
code4love <2548340423@qq.com>
2024-07-27 15:10:30 +0000

    add cute to networkmanger group

--- configuration.nix
+++ configuration.nix
@@ -72,7 +72,7 @@
 	users.cute = {
 	    isNormalUser = true;
 	    home = "/home/cute";
-	    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+	    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
 	    packages = with pkgs; [
 	      firefox
 	      tree
