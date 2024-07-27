8bc5708874eb5e2d2d2ca9cf98c5d7244d4fc786
code4love <2548340423@qq.com>
2024-07-27 14:01:33 +0000

    enable gnome

--- configuration.nix
+++ configuration.nix
@@ -36,6 +36,11 @@
 
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
+  services.xserver = {
+	enable = true;
+	displayManager.gdm.enable = true;
+	desktopManager.gnome.enable = true;
+  };
 
 
   
