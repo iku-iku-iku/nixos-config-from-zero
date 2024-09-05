43286d3c18981cddc2ec18c9371e45a372707b19
code4love <2548340423@qq.com>
2024-09-05 14:19:39 +0800

    modularized configuration

---
+++ system_packages.nix
@@ -0,0 +1,9 @@
+{pkgs}:
+with pkgs; [
+  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  wget
+  git
+  fish # shell
+  alejandra # nix formatter
+  libgcc
+]
